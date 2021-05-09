b0f86f538304fa589b009ce42aec546d9993dac3
Carlos Colín Eusebio <colincarlos015@gmail.com>
2021-05-08 22:17:58 -0500

    Revert "Primera Versión"
    This reverts commit 7baf7e1a5ca03a3f366c90944443c26ed23bc51b.

--- TheAudioDBAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TheAudioDBAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- TheAudioDBAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace TheAudioDBAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
